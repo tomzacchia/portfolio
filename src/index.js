649a249616002cf82cd25d98b86f2f5756e3f365
tomzacchia <tomzacchia@live.com>
2020-10-28 15:42:22 -0400

    Add typewriter to intro

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-import "./styles/styles.scss";
+import "./styles/index.scss";
 
 import img from "./resources/diamon-clothing-desktop.png";
 
-document.querySelector("img").src = img;
+// Work scrolling
+const viewWorkElement = document.querySelector("#work-anchor");
+
+viewWorkElement.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const cardContainer = document.querySelector(".work");
+
+  cardContainer.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// Typewritter
+
+const typwriter = new Typewriter("#intro-dynamic", {
+  loop: false,
+});
+
+const typewriterDeleteSpeed = 150;
+
+typwriter
+  .typeString("Exercise Enthusiast")
+  .pauseFor(typewriterDeleteSpeed)
+  .changeDeleteSpeed(10)
+  .deleteChars(30)
+  .typeString("Mech Eng Graduate")
+  .pauseFor(typewriterDeleteSpeed)
+  .deleteChars(30)
+  .typeString("Front End Developer")
+  .start();
