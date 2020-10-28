9fe24f1a9aa30f983621c85a74f8b63c8225d648
tomzacchia <tomzacchia@live.com>
2020-10-28 15:27:03 -0400

    Add webpack configuration

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles/styles.scss";
+
+import img from "./resources/diamon-clothing-desktop.png";
+
+document.querySelector("img").src = img;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: { loader: "file-loader" },
+      },
+      {
+        test: /\.svg/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
