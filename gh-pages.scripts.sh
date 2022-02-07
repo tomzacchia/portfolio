309072114b249ae9c5d34c5fad42967f845d841f
tomzacchia <tomzacchia@live.com>
2022-02-06 18:02:39 -0800

    Update codepen link

--- gh-pages.scripts.sh
+++ gh-pages.scripts.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
+# https://gist.github.com/cobyism/4730490
 echo "Pushing build to gh-pages subtree"
-git subtree push --prefix $1 origin gh-pages
+git subtree push --prefix dist origin gh-pages
